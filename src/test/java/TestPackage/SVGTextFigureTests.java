6227d68434189780171e194e8bd2184bbd907c18
Frederik B Roth <frederikbroth@gmail.com>
2021-12-04 23:11:25 +0100

    Added Unit Tests for SVGTextAreaFigure and SVGTextFigure

---
+++ src/test/java/TestPackage/SVGTextFigureTests.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestPackage;
+import org.jhotdraw.samples.svg.figures.SVGTextFigure;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SVGTextFigureTests {
+  @Test
+  public void defaultFontSize() {
+    float defaultFont = 12.0F;
+    SVGTextFigure textFigure = new SVGTextFigure();
+    float sum = textFigure.getFontSize();
+    assertEquals(defaultFont, sum, 0.0002);
+  }
+  @Test
+  public void changeFontSize(){
+    float defaultFont = 12.0F;
+    SVGTextFigure textFigure = new SVGTextFigure();
+    float size = textFigure.getFontSize();
+    assertEquals(defaultFont, size, 0.0002);
+    textFigure.setFontSize(20);
+    size = textFigure.getFontSize();
+    assertEquals(20.0F, size, 0.0002);
+  }
+}
